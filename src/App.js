f958c9906c3949507d4cf3433f5bbc740de2545a
Muhammadjaafer <mohamadjafer14235@gmail.com>
2022-07-30 03:39:16 +0300

    trying ip address api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Canvas from "./components/Canvas";
 import ColorPicker from "./components/ColorPicker";
 
 function App() {
   const [currentColor, setCurrentColor] = useState();
+  const [ip, setip] = useState(null);
+  useEffect(() => {
+    fetch(
+      "https://ipgeolocation.abstractapi.com/v1/?api_key=5641164e32384fae90647be10109bebf"
+    ).then((res) => {
+      res.json().then((data) => {
+        setip(data.ip_address);
+      });
+    });
+  }, []);
   return (
     <div className="App">
       <h1 className="title">r/Place Clone</h1>
@@ -13,6 +23,7 @@ function App() {
         setCurrentColor={setCurrentColor}
         currentColor={currentColor}
       />
+      <p className="ip">{ip}</p>
     </div>
   );
 }
